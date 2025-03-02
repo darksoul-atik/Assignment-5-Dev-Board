d5a1ea85f10e81da7cf8de1cf3b9364236743087
DarKSoul Atik <anantoshahrear10@gmail.com>
2025-03-02 20:17:55 +0600

    color chng js done

---
+++ scripts/bodyColorChange.js
@@ -0,0 +1,18 @@
+document
+  .getElementById("btnColorChange")
+  .addEventListener("click", function (event) {
+    event.preventDefault();
+    const body = document.getElementById("body");
+    let randomColor = Math.random().toString(16);
+    randomColor = randomColor.slice(2, 8);
+
+    if (randomColor.length < 6) {
+      while (randomColor.length < 6) {
+        randomColor = randomColor + "0";
+      }
+    }
+    randomColor = "#" + randomColor;
+
+    body.classList.remove("bg-slate-100");
+    body.style.backgroundColor = randomColor;
+  });
---
+++ scripts/tempCodeRunnerFile.js
@@ -0,0 +1,5 @@
+document.getElementById("btnColorChange").addEventListener("click",function (event) {
+//     event.defaultPrevented();
+
+//     const random = Math.floor(Math.random()*100000);
+// });
